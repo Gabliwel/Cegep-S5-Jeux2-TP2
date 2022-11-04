beff299a7629484afbcc48ae4f08b63f8964ea40
Cédric Bouchard <58229616+Waza-Ari2978@users.noreply.github.com>
2022-11-04 12:43:27 -0400

    spawn behavior completed

---
+++ tp2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject wizard;
+    [SerializeField] private string towerTag;
+
+    private static int maxWizards = 50;
+    private GameObject[] wizards = new GameObject[maxWizards];
+    private int spawnInterval = 5;
+    private float currentTimer = 0f;
+    private GameObject[] towers;
+
+    void Start()
+    {
+        preLoadActors();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manageSpawn();
+    }
+
+    void manageSpawn()
+    {
+        int randomSpawner = Random.Range(1, towers.Length);
+        if (currentTimer >= spawnInterval)
+        {
+            for (int i = 0; i < maxWizards; i++)
+            {
+                if (!wizards[i].activeSelf)
+                {
+                    if (towers[randomSpawner].activeSelf)
+                    {
+                        wizards[i].transform.position = towers[randomSpawner].transform.position;
+                        wizards[i].SetActive(true);
+                        currentTimer = 0;
+                    }
+                    break;
+                }
+            }
+        }
+
+        currentTimer += Time.deltaTime;
+    }
+
+    void preLoadActors()
+    {
+        for (int i = 0; i < maxWizards; i++)
+        {
+            wizards[i] = Instantiate(wizard);
+            wizards[i].transform.parent = gameObject.transform;
+            wizards[i].SetActive(false);
+        }
+
+        towers = GameObject.FindGameObjectsWithTag(towerTag);
+    }
+}
